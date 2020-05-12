6badc5fb1a9c72ca0c9416c6b8ba9c82a76ed71c
devleandrodias <leandrodbdias@gmail.com>
2020-05-12 16:27:32 -0300

    formulário, simples de POST com váriaveis super globais em php

---
+++ formularios/dados.php
@@ -0,0 +1,6 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+
+print "Olá Sr. ${nome}, foi enviado um email para ${email}";
---
+++ formularios/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulário</title>
+</head>
+
+<body>
+  <form action="dados.php" method="POST">
+    Nome: <input type="text" name="nome"> <br />
+    Email: <input type="email" name="email"> <br />
+    <button type="submit">Enviar</button>
+  </form>
+</body>
+
+</html>
