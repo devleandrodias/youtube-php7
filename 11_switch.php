11dc4011eccd003c068189eafd65ad649cc03f42
devleandrodias <leandrodbdias@gmail.com>
2020-05-12 14:25:29 -0300

    switch case com php

---
+++ 11_switch.php
@@ -0,0 +1,20 @@
+<?php
+
+// Switch
+
+$cor = "cinza";
+
+switch ($cor):
+  case 'amarelo':
+    print 'A cor é amarela';
+    break;
+  case 'verde':
+    print 'A cor é verde';
+    break;
+  case 'cinza':
+    print 'A cor é cinza';
+    break;
+  default:
+    print "Cor não identificada";
+    break;
+endswitch;
