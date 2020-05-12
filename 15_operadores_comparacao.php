86b67c9d8edf3340883417e1244f191ea7caf4b8
devleandrodias <leandrodbdias@gmail.com>
2020-05-12 15:05:19 -0300

    Operadores lógicos em php

--- 15_operadores_comparacao.php
+++ 15_operadores_comparacao.php
@@ -11,5 +11,5 @@
  * >
  * <=
  * >=
- * <=>
+ * <=> // comparacoes combinadas
  */
---
+++ 16_operadores_logicos.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Operadores lógicos, Igual JavaScript
+ *
+ * && - and (e)
+ * || - or (ou)
+ * ou exclusivo (xor)
+ * ! (negação)
+ */
