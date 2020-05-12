2968781dd38317966dc5f2cc15b4e2f9c70f0248
devleandrodias <leandrodbdias@gmail.com>
2020-05-12 14:32:02 -0300

    operadores aritiméticos em php

---
+++ 12_operadores_aritimeticos.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * adição, subtração,
+ * multiplicação, divisão,
+ * módulo, exponenciação
+ */
+
+print 3 + 5;
+print "<hr/>";
+
+print 3 - 5;
+print "<hr/>";
+
+print 3 * 5;
+print "<hr/>";
+
+print 3 / 5;
+print "<hr/>";
+
+print 3 % 5;
+print "<hr/>";
+
+print 3 ** 5;
+print "<hr/>";
