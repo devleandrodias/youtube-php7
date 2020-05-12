e7f2dbedf377c37a3c5f0807b4311218365200c7
devleandrodias <leandrodbdias@gmail.com>
2020-05-12 16:10:13 -0300

    váriaveis super globais

---
+++ 22_variaveis_super_globais.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * $GLOBALS - usada para acessar váriaveis globais em qualquer lugar script php
+ * $_SERVER
+ * $_REQUEST
+ * $_POST
+ * $_GET
+ * $_FILES
+ * $_ENV
+ * $_COOKIE
+ * $_SESSION
+ */
+
+print $_SERVER['PHP_SELF'] . "<br/>";
+print $_SERVER['SERVER_NAME'] . "<br/>";
+print $_SERVER['SCRIPT_FILENAME'] . "<br/>";
+print $_SERVER['DOCUMENT_ROOT'] . "<br/>";
+print $_SERVER['SERVER_PORT'] . "<br/>";
+print $_SERVER['REMOTE_ADDR'] . "<br/>";
