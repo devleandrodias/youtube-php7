7750deb9ef750d398683ea6cba71ba71c4608398
devleandrodias <leandrodbdias@gmail.com>
2020-05-12 14:46:38 -0300

    operadores incremento e descremento

---
+++ 13_operadores_incremento_decremento.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Incrementadores e Decrementadores
+ * Forma préfixadas e pósfixadas
+ */
+
+$valorPre = 20;
+$valorPos = 40;
+
+print ++$valorPre;
+print "<br/>";
+
+print $valorPos++;
+print "<br/>";
+
+print "<hr/>";
+
+print --$valorPre;
+print "<br/>";
+
+print $valorPos--;
+print "<br/>";
