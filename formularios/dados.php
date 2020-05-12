f95ef479390204c2559b53827e8313df5e722be7
devleandrodias <leandrodbdias@gmail.com>
2020-05-12 16:34:16 -0300

    outra forma de sintaxe

--- formularios/dados.php
+++ formularios/dados.php
@@ -3,4 +3,5 @@
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 
-print "Olá Sr. ${nome}, foi enviado um email para ${email}";
+print "Olá Sr. ${nome}, foi enviado um email para ${email} <br/>";
+print "Olá Sr. " . $_POST['nome'] . ", foi enviado um email para " . $_POST['email'];
