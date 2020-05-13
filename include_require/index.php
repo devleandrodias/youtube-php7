bf3374955117c8a8859862cb5c4e94895a4df12f
devleandrodias <leandrodbdias@gmail.com>
2020-05-13 08:37:02 -0300

    Include e Require no php

---
+++ include_require/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Index</title>
+</head>
+
+<?php include 'header.php' // Náo interrompe mostra warning se nao encontrar
+?>
+
+<!-- include_once se ele já tiver cido incluido não inclui novamente -->
+
+<body>
+  Include / Require
+  <br />
+</body>
+
+<?php require 'footer.php'  // Interrompe tudo se nao encontrar
+?>
+
+</html>
