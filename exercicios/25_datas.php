dd7e4431e5cd05f97b361713a539ffc13a8ee637
devleandrodias <leandrodbdias@gmail.com>
2020-05-14 11:55:03 -0300

    Trabalhando com datas em php

---
+++ exercicios/25_datas.php
@@ -0,0 +1,39 @@
+<?php
+date_default_timezone_get('America/Sao_Paulo');
+print date('d');
+print "<br/>";
+
+print date('D');
+print "<br/>";
+
+print date('m');
+print "<br/>";
+
+print date('M');
+print "<br/>";
+
+print date('y');
+print "<br/>";
+
+print date('Y');
+print "<br/>";
+
+print date('d/m/y');
+print "<br/>";
+
+print date('l');
+print "<br/>";
+
+print date('d/m/Y h');
+print "<br/>";
+
+print date('d/m/Y H:i:s A');
+print "<br/>";
+
+$date = date('Y-m-d');
+print $date;
+print "<br/>";
+
+$dateTime = date('Y-m-d H:i:s');
+print $dateTime;
+print "<br/>";
